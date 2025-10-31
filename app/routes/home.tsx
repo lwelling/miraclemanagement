5a28eedf926ee80806a626f2a9c3da7a6f4ef626
Lucas Welling <lucaswelling1@gmail.com>
2025-10-31 16:33:48 -0700

    Removed boilerplate code, added simple landing page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,7 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white text-gray-900">
+      <h1 className="text-3xl font-semibold">welcome to miracle management</h1>
+    </main>
+  );
 }
